1cf17ddfd474f0753ac6a36e81a2a46f997171f1
Adithak_99 <94104632+Adithaklm@users.noreply.github.com>
2022-05-17 20:42:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -100,18 +100,19 @@ async def next_page(bot, query):
     if n_offset == 0:
         btn.append(
             [InlineKeyboardButton(" ʙᴀᴄᴋ", callback_data=f"next_{req}_{key}_{off_set}"),
-             InlineKeyboardButton(f"📦 Pages {round(int(offset) / 10) + 1} / {round(total / 10)}",
-                                  callback_data="pages")]
+             InlineKeyboardButton(f"📦 Pages {round(int(offset) / 10) + 1} - {round(total / 10)}", callback_data="pages"), 
+             InlineKeyboardButton("ᴅᴇʟᴇᴛᴇ", callback_data="close_pages")]
         )
     elif off_set is None:
         btn.append(
-            [InlineKeyboardButton(f"📦 {round(int(offset) / 10) + 1} / {round(total / 10)}", callback_data="pages"),
+            [InlineKeyboardButton("ᴘᴀɢᴇ", callback_data="pages"),
+             InlineKeyboardButton(f"📦 {round(int(offset) / 10) + 1} - {round(total / 10)}", callback_data="pages"),
              InlineKeyboardButton("ɴᴇxᴛ ", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
         btn.append(
             [
                 InlineKeyboardButton(" ʙᴀᴄᴋ", callback_data=f"next_{req}_{key}_{off_set}"),
-                InlineKeyboardButton(f"ᴘᴀɢᴇ {round(int(offset) / 10) + 1} / {round(total / 10)}", callback_data="pages"),
+                InlineKeyboardButton(f"ᴘᴀɢᴇ {round(int(offset) / 10) + 1} - {round(total / 10)}", callback_data="pages"),
                 InlineKeyboardButton("ɴᴇxᴛ ", callback_data=f"next_{req}_{key}_{n_offset}")
             ],
         )
