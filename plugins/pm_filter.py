566d0ddd741e52965417a2ae41ecbd06b6fef21c
Adithak_99 <94104632+Adithaklm@users.noreply.github.com>
2022-05-23 00:12:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -829,7 +829,7 @@ async def advantage_spell_chok(msg):
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist))  # removing duplicates
     if not movielist:
-        k = await msg.reply("I couldn't find anything related to that. Check your spelling")
+    await msg.reply("I couldn't find anything related to that. Check your spelling")
     buttons = [[
             InlineKeyboardButton('🚸 ᴅᴇʟᴇᴛᴇ', url="https://t.me/cinimalinksz")
     ]]
